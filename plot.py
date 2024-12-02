a94c61713aad822ee7d9352ff067da52ef356b22
ShoBob <shobob29@gmail.com>
2024-12-02 00:39:34 -0800

    L1 norm

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the Excel file
-file_path = '/Users/s_wada/Documents/Stanford_Research/Research Meeting/0916/Linear_advection_L2norm.xlsx'
+file_path = '/Users/s_wada/Documents/Stanford_Research/Research Meeting/0923/euler_L2norm.xlsx'
 data = pd.read_excel(file_path)
 # data = pd.read_excel(file_path, sheet_name=1)
 
@@ -12,16 +12,16 @@ L2norm4th = data.iloc[:, 1]  # Second column for points
 fouth_o = data.iloc[:, 2]  # Third column for line
 fifth_o = data.iloc[:,4]
 
-L2norm3rd = data.iloc[:,5]
+L2norm1st = data.iloc[:,5]
 L2norm2nd = data.iloc[:,6]
-fourth_o2 = data.iloc[:,7]
-thrid_o2 = data.iloc[:,8]
+third_o2 = data.iloc[:,8]
+second_o2 = data.iloc[:,9]
 
 # Create the plot
 plt.figure(figsize=(8, 6))
-plt.scatter(x, L2norm4th, color='b', label='p4', marker='o')  # Plot points
+plt.scatter(x, L2norm4th, color='b', label='p3', marker='o')  # Plot points
 # plt.plot(x, fouth_o, color='r', label='4th order')  # Plot line
-plt.plot(x, fifth_o, color='r', label='5th order')  # Plot line
+plt.plot(x, fouth_o, color='r', label='4th order')  # Plot line
 # Set logarithmic scale
 plt.xscale('log')
 plt.yscale('log')
@@ -38,8 +38,8 @@ plt.show()
 
 
 plt.figure(figsize=(8, 6))
-plt.scatter(x, L2norm3rd, color='b', label='p3', marker='o')  # Plot points
-plt.plot(x, fourth_o2, color='r', label='4th order')  # Plot line
+plt.scatter(x, L2norm1st, color='b', label='p1', marker='o')  # Plot points
+plt.plot(x, second_o2, color='r', label='2nd order')  # Plot line
 # Set logarithmic scale
 plt.xscale('log')
 plt.yscale('log')
@@ -56,7 +56,7 @@ plt.show()
 
 plt.figure(figsize=(8, 6))
 plt.scatter(x, L2norm2nd, color='b', label='p2', marker='o')  # Plot points
-plt.plot(x, thrid_o2, color='r', label='3rd order')  # Plot line
+plt.plot(x, third_o2, color='r', label='3rd order')  # Plot line
 # Set logarithmic scale
 plt.xscale('log')
 plt.yscale('log')
